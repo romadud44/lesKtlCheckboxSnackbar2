cd323c6cf43b1553bc5ea21f26977e1baa3d8c12
romadud44 <roman_crt-service@mail.ru>
2024-07-14 15:50:46 +0300

    commit

--- app/src/main/java/com/example/lesktlcheckboxsnackbar2/MainActivity.kt
+++ app/src/main/java/com/example/lesktlcheckboxsnackbar2/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.lesktlcheckboxsnackbar2
 
 import android.os.Bundle
 import android.widget.CheckBox
-import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -12,27 +11,27 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var toolbarMain: Toolbar
-
     private lateinit var checkCB: CheckBox
     private lateinit var outTV: TextView
     private lateinit var infoPddTV: TextView
+    private fun findId(){
+        toolbarMain = findViewById(R.id.toolbarMain)
+        setSupportActionBar(toolbarMain)
+        title = "Правила дорожного движения"
+        toolbarMain.subtitle = "версия 1.0"
+
+        checkCB = findViewById(R.id.checkCB)
+        outTV = findViewById(R.id.outTV)
+        infoPddTV = findViewById(R.id.infoPddTV)
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        toolbarMain = findViewById(R.id.toolbarMain)
-        setSupportActionBar(toolbarMain)
-        title = "Правила дорожного движения"
-        toolbarMain.subtitle = "версия 1.0"
-
-
-        checkCB = findViewById(R.id.checkCB)
-        outTV = findViewById(R.id.outTV)
-        infoPddTV = findViewById(R.id.infoPddTV)
+        findId()
 
         checkCB.setOnClickListener{
             if(checkCB.isChecked){
@@ -43,7 +42,6 @@ class MainActivity : AppCompatActivity() {
                 infoPddTV.text = ""
             }
         }
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -51,3 +49,5 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
+
